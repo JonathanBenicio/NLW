de62a0a9d0aca2cb5d5930ab2a4394e4791c632a
Jonathan Benicio de Lima Oliveira <45011024+JonathanBenicio@users.noreply.github.com>
2020-06-09 04:35:16 -0300

    preenchendo seach-results.html com dados do db

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const server = express()
 
+// pegar o banco de dados
+
+const db = require("./database/db.js")
+
 // conficuração pasta publica
 server.use(express.static("public"))
 
@@ -16,16 +20,28 @@ nunjucks.configure("src/views", {
 // req: Requisição
 // res: Resposta
 server.get("/", (req, res) => {
-    return res.render("index.html", {title:"te"})
+    return res.render("index.html", { title: "te" })
 })
 server.get("/create-point", (req, res) => {
+    
+    
+    
     return res.render("create-point.html")
 })
 server.get("/search", (req, res) => {
-    return res.render("search-results.html")
+    //pegar os dados do banco de dados
+    db.all(`SELECT * FROM places`, function (err, rows) {
+        if (err) {
+            return colonso.log(err)
+        }
+
+        const total = rows.length
+
+        //mostrar a pagina HTML com os dados do banco de dados
+        return res.render("search-results.html", { places: rows, total })
+    })
 })
 
 // ligar servidor
 server.listen(3000)
 
- 
