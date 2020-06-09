ed10ede8689e8a1ac50c517cf661c2a357321026
Jonathan Benicio de Lima Oliveira <45011024+JonathanBenicio@users.noreply.github.com>
2020-06-09 06:39:17 -0300

    Day 5 finalizado

--- src/server.js
+++ src/server.js
@@ -8,6 +8,9 @@ const db = require("./database/db.js")
 // conficuração pasta publica
 server.use(express.static("public"))
 
+//habilitar o uso do req.body na nossa aplicação
+server.use(express.urlencoded({ extended: true }))
+
 // utilizando template engine
 const nunjucks = require("nunjucks")
 nunjucks.configure("src/views", {
@@ -23,14 +26,61 @@ server.get("/", (req, res) => {
     return res.render("index.html", { title: "te" })
 })
 server.get("/create-point", (req, res) => {
-    
-    
-    
+
+    //req.query: Query String dentro da url
+    //console.log(req.query)
+
     return res.render("create-point.html")
+    
 })
+
+server.post("/savepoint", (req, res) => {
+    //req.body: O corpo do nosso formulario
+    // console.log(req.body)
+
+    const query = `
+    INSERT INTO places (
+        image,
+        name,
+        address,
+        address2,
+        state,
+        city,
+        items            
+    ) VALUES (?,?,?,?,?,?,?);
+    `
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+    ]
+
+    function afterInsertData(err) {
+        if (err) {
+            console.log(err)
+            return res.send("Erro no cadastro!")
+        }
+
+        // console.log("Cadastrado com sucesso")
+        // console.log(this)
+
+        return res.render("create-point.html", { saved: true})
+    }
+
+    db.run(query, values, afterInsertData)
+})
+
 server.get("/search", (req, res) => {
     //pegar os dados do banco de dados
-    db.all(`SELECT * FROM places`, function (err, rows) {
+    const search = req.query.search
+
+    if(search == "") return res.render("search-results.html", {total: 0})
+
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function (err, rows) {
         if (err) {
             return colonso.log(err)
         }
